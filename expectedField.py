c2fbc5abf6c02e2f27c20660ff81496ce5e2b54a
André Torres <andregtorres@gmail.com>
2018-09-23 21:04:55 +0100

    Get theroetical TF coils effect on mirnovs

--- expectedField.py
+++ expectedField.py
@@ -2,42 +2,53 @@ from field import *
 from getMirnov import *
 from scipy.constants import mu_0
 #SDAS
-shotnr=42952;
+shotV=42952
+shotH=42966
+shotP=43066
 #Coil signals
-vert, times, tbs = getSignal(ch_vert, shotnr )
+vert, times, tbs = getSignal(ch_vert, shotV )
+hor, times, tbs = getSignal(ch_hor, shotH )
+prim, times, tbs = getSignal(ch_prim, shotP )
 #mirnov signals
-times, data = getMirnovs(shotnr,mirnv,True)
+times, dataV = getMirnovs(shotV,mirnv,True)
+times, dataH = getMirnovs(shotH,mirnv,True)
+times, dataP = getMirnovs(shotP,mirnv,True)
 
-radius=9.35 #cm
-angle=345. - 30.*np.arange(12)
-geometryZ=radius*np.sin(np.radians(angle)) #positions of the mirnovs
-geometryR=radius*np.cos(np.radians(angle))
+#computes the flux on each mirnov normalized for a 1 amp current running on coils in Rw,Zw
+def getMirnovFlux(Rw_,Zw_,polarity,windings):
+    #mirnov positions
+    radius=9.35 #cm
+    angle=345. - 30.*np.arange(12)
+    geometryZ=radius*np.sin(np.radians(angle)) #positions of the mirnovs
+    geometryR=radius*np.cos(np.radians(angle))
+    #loop on the mirnovs
+    Hr=np.zeros(len(angle))
+    Hz=np.zeros(len(angle))
+    i=0
+    for r,z in zip(geometryR,geometryZ):
+        #loop on the PFCs
+        for Rw,Zw, sign in zip(Rw_,Zw_,polarity):
+            coilHr, coilHz= biotsavart((r+46.)*1e-2, z*1e-2, Rw*1e-2,Zw*1e-2,1.0) #46.
+            #coilHr, coilHz= Hcoil((r+46.)*1e-2, z*1e-2, Rw*1e-2,Zw*1e-2) #46.
+            Hr[i]+=sign*coilHr
+            Hz[i]+=sign*coilHz
+        i+=1
+    Hr=np.asarray(Hr)
+    Hz=np.asarray(Hz)
+    Hp=-Hr*np.sin(np.radians(angle))+Hz*np.cos(np.radians(angle))
+    #Hp=-Hr*np.cos(np.radians(angle))+Hz*np.sin(np.radians(angle))
+    return Hp*windings*50*49e-6
 
-Hr=np.zeros(len(angle))
-Hz=np.zeros(len(angle))
-for r,z in zip(geometryR,geometryZ):
-    for Rw,Zw, sign in zip([58.,58.,35.,35.],[-7.,7.,-7.,7.],[-1.,-1.,1.,1.]):
-        coilHr, coilHz= biotsavart(r+46., z, Rw,Zw,1.0) #46.
-        Hr+=sign*coilHr
-        Hz+=sign*coilHz
-Hr=np.asarray(Hr)
-Hz=np.asarray(Hz)
-Hp=Hr*np.sin(np.radians(angle))-Hz*np.cos(np.radians(angle))
-Htot=np.sqrt(Hr**2+Hz**2)
-Htot*340*5
-plt.plot(np.arange(12)+1,Htot)
+V=getMirnovFlux([58.,58.,35.,35.],[-7.,7.,-7.,7.],[-1.,-1.,1.,1.],5)*340
+H=getMirnovFlux([58.,58.],[-7.,7.],[+1.,-1.],4)*260
+P=getMirnovFlux([62.,62.],[-13.,13.],[+1.,1.],14)*157
 
-plt.plot(np.arange(12)+1,np.array(data)[:,5001])
-Hz*340*5
-Hp*340
+plt.plot(np.arange(12)+1,-V*1e8)
+plt.plot(np.arange(12)+1,np.array(dataV)[:,5001]*1e8)
 
-Hp=Hp*50.*49e-6
-Vf=5.*np.outer(np.asarray(Hp).T,vert)
+plt.plot(np.arange(12)+1,H*1e8)
+plt.plot(np.arange(12)+1,np.array(dataH)[:,5001]*1e8)
 
 
-for mirnov in Vf[0:3]:
-    plt.plot(times,mirnov)
-    print np.max(mirnov)
-for mirnov in [data[0], data[11], data[5], data[1], data[4]]:
-    plt.plot(times,mirnov)
-    print np.max(mirnov)
+plt.plot(np.arange(12)+1,P*1e8)
+plt.plot(np.arange(12)+1,np.array(dataP)[:,5001]*1e8)
--- field.py
+++ field.py
@@ -19,40 +19,76 @@ def Hcoil (R, Z, Rw, Zw):
     flux=ss_sqr*((1.-m/2.)*K-E)
     hR  = dZ  / R * (num_r / den * E - K) / ss_sqr  / 2. / np.pi
     hZ = (K + num_z / den * E ) / ss_sqr  / 2. / np.pi
-    return hR,hZ,flux
+    return hR*mu_0,hZ*mu_0
 
-def biotsavart(r,z,Rw,zw,I,N=400):
+
+def biotsavart(r,z,Rw,zw,I,N=100):
     point=np.array((r,0,z))
     B=np.zeros(3)
     for phi in np.linspace(0,2*np.pi,N):
         w=np.array([Rw*np.cos(phi),Rw*np.sin(phi),zw])
+        w1=np.array([Rw*np.cos(phi+(2*np.pi)/N),Rw*np.sin(phi+(2*np.pi)/N),zw])
         dist=point-w
         d=np.sqrt(np.sum([i**2 for i in dist]))
         dl=np.array([-np.sin(phi), np.cos(phi), 0])*2.*np.pi*Rw/N
+        dl2=w1-w
         cross=np.cross(dl,dist)
         B=B+(cross/d**3)
     B=B*mu_0/4./np.pi*I
-    return np.sqrt(B[0]**2+B[1]**2), B[2]
+    return B[0], B[2]
 
 def biotSavartCyl(r,z,Rw,Zw,I):
     d=np.sqrt((r-Rw)**2+ (z-Zw)**2)
     B=mu_0/4./np.pi*I/(d**2)
-    alpha=np.arctan2((z-Zw),(r-Rw))
-    Br=B*np.sin(alpha)
-    Bz=B*np.cos(alpha)
+    alpha=np.arctan((z-Zw)/(r-Rw))
+    Bz=B*np.sin(alpha)
+    Br=-B*np.cos(alpha)
     return Br, Bz
+'''
+R=np.linspace(30,60, 20)
+Z=np.linspace(-10,10, 20)
+Br=np.zeros(shape=(20,20))
+Bz=np.zeros(shape=(20,20))
 
-R=np.linspace(20,70)
-Z=np.linspace(-10,10)
-Br=np.zeros(50)
-Bz=np.zeros(50)
 for Rw,Zw, sign in zip([58.,58.,35.,35.],[-7.,7.,-7.,7.],[-1.,-1.,1.,1.]):
-    for i in range(50):
-        br,bz=biotSavartCyl(50*1e-2, Z[i]*1e-2, Rw*1e-2,Zw*1e-2,340*sign) #46.
-        Br[i] = Br[i] + br
-        Bz[i] = Bz[i] + bz
+    for i in range(20):
+        print i
+        for j in range(20):
+            br,bz=biotsavart(R[i]*1e-2, Z[j]*1e-2, Rw*1e-2,Zw*1e-2,340*sign) #46.
+            Br[i][j] = Br[i][j] + br
+            Bz[i][j] = Bz[i][j] + bz
 
+
+Br2=np.zeros(shape=(20,20))
+Bz2=np.zeros(shape=(20,20))
+for Rw,Zw, sign in zip([58.,58.,35.,35.],[-7.,7.,-7.,7.],[-1.,-1.,1.,1.]):
+    for i in range(20):
+        print i
+        for j in range(20):
+            br2,bz2 =Hcoil(R[i]*1e-2, Z[j]*1e-2, Rw*1e-2,Zw*1e-2) #46.
+            Br2[i][j] = Br2[i][j] + br2*340*sign
+            Bz2[i][j] = Bz2[i][j] + bz2*340*sign
+
+Br3=np.zeros(shape=(20,20))
+Bz3=np.zeros(shape=(20,20))
+for Rw,Zw, sign in zip([58.,58.,35.,35.],[-7.,7.,-7.,7.],[-1.,-1.,1.,1.]):
+    for i in range(20):
+        print i
+        for j in range(20):
+            br3,bz3=biotSavartCyl(R[i]*1e-2, Z[j]*1e-2, Rw*1e-2,Zw*1e-2,340.*sign) #46.
+            Br3[i][j] = Br3[i][j] + br3
+            Bz3[i][j] = Bz3[i][j] + bz3
+print Bz
+print Bz2*mu_0
 import matplotlib.pyplot as plt
+print Bz2*mu_0
+plt.contourf(R,Z,Bz2*mu_0, levels=np.linspace(-1e-2,0,200))
+plt.plot(Bz[10]*1000)
+plt.plot(Bz2[10]*mu_0*1000)
+plt.contourf(R,Z,np.sqrt(Bz**2+Br**2))#, levels=np.linspace(-1e-2,0,200)
+plt.quiver(Br2,Bz2)
+
 plt.plot(Z,np.sqrt(Br**2+Bz**2)*1000)
 plt.plot(Z,Br*1000)
 plt.plot(Z,Bz*1000)
+'''
