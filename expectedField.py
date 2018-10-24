803f461f7cc8b8ed2d684d2f94129c554178d8ee
André Torres <andregtorres@gmail.com>
2018-10-25 00:26:38 +0200

    add brut force optimization

--- expectedField.py
+++ expectedField.py
@@ -48,14 +48,80 @@ P=getMirnovFlux([62.,62.],[-13.,13.],[+1.,1.],14)*157
 def flatTops (data):
     return np.asarray([np.mean(np.array(i)[4000:6000]) for i in data])
 
+ftV=flatTops(dataV)
+squareSums=[]
+for dx in np.arange(-0.5,0.5,0.1):
+    err1=[]
+    for dy in np.arange(-1.,1.,0.1):
+        V=getMirnovFlux([58.+dx,58.+dx,35.+dx,35.+dx],[-7.+dy,7.+dy,-7.+dy,7.+dy],[-1.,-1.,1.,1.],5)*340
+        err=((ftV-(-V))**2)
+        err1.append(err.sum())
+    squareSums.append(np.asarray(err1))
+
+np.asarray(squareSums)
+%matplotlib qt4
+plt.figure()
+plt.contourf(np.arange(-0.5,0.5,0.1),np.arange(-1.,1.,0.1),np.transpose(np.asarray(squareSums)))
+plt.colorbar()
+plt.xlabel("dR")
+plt.ylabel("dZ")
+
+V2=getMirnovFlux([58.-0.25,58.-0.25,35.-0.25,35.-0.25],[-7.,7.+1,-7.,7.+1],[-1.,-1.,1.,1.],5)*340
+H2=getMirnovFlux([58-3.5,58-5],[-7-1,7.+5],[+1.,-1.],4)*260
+H0=getMirnovFlux([58.,58.],[-7.,7.],[+1.,-1.],4, biotSavart=False)*260
+
+
+ftH=flatTops(dataH)
+xx1=np.arange(-4,4,0.5)
+xx2=np.arange(-4,4,0.5)
+yy1=np.arange(-4,4,0.5)
+yy2=np.arange(-4,4,0.5)
+ii=np.arange(-10,10,2)
+error_min=[1,0,0,0,0,0]
+for di in ii:
+    print di
+    for dx1 in xx1:
+        for dx2 in xx2:
+            for dy1 in yy1:
+                for dy2 in yy2:
+                    H=getMirnovFlux([58.+dx1,58.+dx2],[-7+dy1,7.+dy2],[+1.,-1.],4, biotSavart=False)*(260+di)
+                    err=np.sqrt(((ftH-H)**2).sum())
+                    if err < error_min[0]:
+                        error_min=[err,dx1,dx2,dy1,dy2,di]
+error_min
+H2=getMirnovFlux([58+error_min[1],58+error_min[2]],[-7+error_min[3],7.+error_min[4]],[+1.,-1.],4)*(260+error_min[5])
+
+'''
+squareSums=[]
+for dx1 in xx1:
+    err1=[]
+    H=getMirnovFlux([58.+dx1,58.+dx2],[-7-1,7.+4],[+1.,-1.],4, biotSavart=False)*260
+    err=((ftH-H)**2)
+    err1.append(err.sum())
+    for dx2 in xx2:
+    squareSums.append(np.asarray(err1))
+
+plt.figure()
+plt.contourf(xx1,xx2,np.transpose(np.log(np.asarray(squareSums))))
+plt.colorbar()
+plt.xlabel("dx1")
+plt.ylabel("dx2")
+'''
+
+
+'''
 plt.figure()
 plt.plot(np.arange(12)+1,-V*1e8)
+plt.plot(np.arange(12)+1,-V2*1e8)
 plt.plot(np.arange(12)+1,flatTops(dataV)*1e8)
-
+'''
 plt.figure()
-plt.plot(np.arange(12)+1,H*1e8)
+plt.plot(np.arange(12)+1,H0*1e8)
 plt.plot(np.arange(12)+1,flatTops(dataH)*1e8)
+plt.plot(np.arange(12)+1,H2*1e8)
 
+'''
 plt.figure()
 plt.plot(np.arange(12)+1,P*1e8)
 plt.plot(np.arange(12)+1,flatTops(dataP)*1e8)
+'''
