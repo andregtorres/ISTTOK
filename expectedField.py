9d9fa1b23823a3797edc1f01604970dc9a0109df
André Torres <andregtorres@gmail.com>
2018-10-24 10:15:49 +0200

    Correct flattop of mirnovs

--- expectedField.py
+++ expectedField.py
@@ -15,7 +15,7 @@ times, dataH = getMirnovs(shotH,mirnv,True)
 times, dataP = getMirnovs(shotP,mirnv,True)
 
 #computes the flux on each mirnov normalized for a 1 amp current running on coils in Rw,Zw
-def getMirnovFlux(Rw_,Zw_,polarity,windings):
+def getMirnovFlux(Rw_,Zw_,polarity,windings,biotSavart=True):
     #mirnov positions
     radius=9.35 #cm
     angle=345. - 30.*np.arange(12)
@@ -28,27 +28,34 @@ def getMirnovFlux(Rw_,Zw_,polarity,windings):
     for r,z in zip(geometryR,geometryZ):
         #loop on the PFCs
         for Rw,Zw, sign in zip(Rw_,Zw_,polarity):
-            coilHr, coilHz= biotsavart((r+46.)*1e-2, z*1e-2, Rw*1e-2,Zw*1e-2,1.0) #46.
-            #coilHr, coilHz= Hcoil((r+46.)*1e-2, z*1e-2, Rw*1e-2,Zw*1e-2) #46.
+            if biotSavart:
+                coilHr, coilHz= biotsavart((r+46.)*1e-2, z*1e-2, Rw*1e-2,Zw*1e-2,1.0) #46.
+            else:
+                coilHr, coilHz= Hcoil((r+46.)*1e-2, z*1e-2, Rw*1e-2,Zw*1e-2) #46.
             Hr[i]+=sign*coilHr
             Hz[i]+=sign*coilHz
         i+=1
     Hr=np.asarray(Hr)
     Hz=np.asarray(Hz)
     Hp=-Hr*np.sin(np.radians(angle))+Hz*np.cos(np.radians(angle))
-    #Hp=-Hr*np.cos(np.radians(angle))+Hz*np.sin(np.radians(angle))
     return Hp*windings*50*49e-6
 
 V=getMirnovFlux([58.,58.,35.,35.],[-7.,7.,-7.,7.],[-1.,-1.,1.,1.],5)*340
 H=getMirnovFlux([58.,58.],[-7.,7.],[+1.,-1.],4)*260
 P=getMirnovFlux([62.,62.],[-13.,13.],[+1.,1.],14)*157
 
+#get te mirnov flat top value with heaviside shots
+def flatTops (data):
+    return np.asarray([np.mean(np.array(i)[4000:6000]) for i in data])
+
+plt.figure()
 plt.plot(np.arange(12)+1,-V*1e8)
-plt.plot(np.arange(12)+1,np.array(dataV)[:,5001]*1e8)
+plt.plot(np.arange(12)+1,flatTops(dataV)*1e8)
 
+plt.figure()
 plt.plot(np.arange(12)+1,H*1e8)
-plt.plot(np.arange(12)+1,np.array(dataH)[:,5001]*1e8)
+plt.plot(np.arange(12)+1,flatTops(dataH)*1e8)
 
-
+plt.figure()
 plt.plot(np.arange(12)+1,P*1e8)
-plt.plot(np.arange(12)+1,np.array(dataP)[:,5001]*1e8)
+plt.plot(np.arange(12)+1,flatTops(dataP)*1e8)
